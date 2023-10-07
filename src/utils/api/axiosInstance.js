85bafce0bfc755a4c16cfff718c5ada25f47c378
salekseevalekseeva <salekseevalekseeva@sberbank.ru>
2023-10-07 23:21:46 +0300

    sprint-3/step-1

--- src/utils/api/axiosInstance.js
+++ src/utils/api/axiosInstance.js
@@ -9,28 +9,23 @@ const options = {
 
 const axiosInstance = axios.create(options)
 
-  axiosInstance.interceptors.response.use(response => response.data, error => {
-    const { response, config } = error
-  console.log("response", response);
-    if (response.status !== 403 && response.data.message !== "jwt expired") {
+axiosInstance.interceptors.response.use(response => response.data, error => {
+  const { response, config } = error
+  if (response.status !== 403 && response.data.message !== "jwt expired") {
+    return Promise.reject(response.data.message)
+  }
+  const refreshToken = localStorage.getItem('refreshToken');
+  return axios.post('/auth/token', {
+    token: refreshToken
+  }, { options })
+    .then((response) => {
+      localStorage.setItem("accessToken", response.data.accessToken)
+      localStorage.setItem("refreshToken", response.data.refreshToken)
+      return axiosInstance(config)
+    })
+    .catch(() => {
       return Promise.reject(response.data.message)
-    }
-    const refreshToken = localStorage.getItem('refreshToken');
-    console.log("refreshToken",refreshToken)
-    // Use a 'clean' instance of axios without the interceptor to refresh the token. No more infinite refresh loop.
-    return axios.post('/auth/token', {
-      token: refreshToken
-    }, options)
-      .then((response) => {
-        // If you are using localStorage, update the token and Authorization header here
-        console.log("response token", response)
-        localStorage.setItem("accessToken", response.data.accessToken)
-        localStorage.setItem("refreshToken", response.data.refreshToken)
-        return axiosInstance(config)
-      })
-      .catch(() => {
-        return Promise.reject(response.data.message)
-      })
-  })
-  
-  export default axiosInstance
+    })
+})
+
+export default axiosInstance
--- src/utils/api/checkResponse.js
+++
@@ -1,5 +0,0 @@
-const checkResponse = (res) => {
-    return res.ok ? res.data : Promise.reject(res);
-};
-
-export default checkResponse;
