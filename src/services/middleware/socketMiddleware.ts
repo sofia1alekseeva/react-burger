b02b5608054be12e6cc76f02ebf15b3989c28fae
salekseevalekseeva <salekseevalekseeva@sberbank.ru>
2023-11-06 00:23:16 +0300

    git cache cleared

--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -1,6 +1,5 @@
 import type { Middleware } from "redux";
-
-import type {  StoreState } from "../../services/index";
+import type { StoreState } from "../../services/index";
 import {
   clearOrdersData,
   setOrdersData,
